6777b1242cd88cbb231a5906b403e33ed04b8256
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-14 21:43:44 +0400

    finish Practice_03

---
+++ Day_02_Types/Practice_03/Practice_03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter number: ");
+
+var userInput = Console.ReadLine();
+int num;
+
+if (int.TryParse(userInput, out num) == true)
+{
+    int pow = (int)Math.Pow(num, 2);
+    Console.WriteLine($"The pow of the entered number is: {pow}");
+}
