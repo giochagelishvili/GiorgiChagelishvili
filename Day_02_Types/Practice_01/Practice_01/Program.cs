72c172ab4c13a7765b030c44cb630d060b0e4b79
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-14 21:40:50 +0400

    finish Practice_01

---
+++ Day_02_Types/Practice_01/Practice_01/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter integer number: ");
+
+// Prompt the user for input
+var userInput = Console.ReadLine();
+int num;
+
+// Check if user input is integer
+if (int.TryParse(userInput, out num) == true)
+{
+    // Check if number is odd or even
+    if (num % 2 == 0)
+    {
+        Console.WriteLine($"Entered number {num} is even.");
+    }
+    else
+    {
+        Console.WriteLine($"Entered number {num} is odd.");
+    }
+}
+
+Console.ReadKey();
