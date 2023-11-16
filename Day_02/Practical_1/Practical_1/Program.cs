b610cc83c2f158e63c241ea0888cacee754eed0f
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-16 15:18:18 +0400

    finish Practical_1

---
+++ Day_02/Practical_1/Practical_1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Enter first number: ");
+int firstNum;
+bool firstInput = int.TryParse(Console.ReadLine(), out firstNum);
+
+Console.WriteLine("Enter second number: ");
+int secondNum;
+bool secondInput = int.TryParse(Console.ReadLine(), out secondNum);
+
+if (firstInput == true && secondInput == true)
+{
+    swapNumbers(firstNum, secondNum);
+}
+
+Console.ReadKey();
+
+static void swapNumbers(int firstNum, int secondNum)
+{
+    firstNum += secondNum;
+    secondNum = firstNum - secondNum;
+    firstNum = firstNum - secondNum;
+
+    Console.WriteLine($"First number: {firstNum}");
+    Console.WriteLine($"Second number: {secondNum}");
+}
+
+static void swapWithThirdVar(int firstNum, int secondNum)
+{
+    int temp = firstNum;
+    firstNum = secondNum;
+    secondNum = temp;
+
+    Console.WriteLine($"First number: {firstNum}");
+    Console.WriteLine($"Second number: {secondNum}");
+}
