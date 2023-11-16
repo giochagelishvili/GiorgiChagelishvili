a1f4586a6b3efa5b1ba38d494a843a8af494e5a6
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-16 15:25:47 +0400

    finish Practical_2

---
+++ Day_02/Practical_2/Practical_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter a year: ");
+int year;
+bool yearInput = int.TryParse(Console.ReadLine(), out year);
+
+if (yearInput == true)
+{
+    bool isLeapYear = checkLeapYear(year);
+    Console.WriteLine(isLeapYear);
+}
+
+static bool checkLeapYear(int year)
+{
+    if (year % 4 == 0 && year % 100 != 0)
+        return true;
+
+    if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+        return true;
+
+    return false;
+}
