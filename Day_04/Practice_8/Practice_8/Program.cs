845347d8ea9d43512eb4d6921ef0b29b12ca2ffb
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-18 20:22:16 +0400

    finish Practice_8

---
+++ Day_04/Practice_8/Practice_8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter a number: ");
+int num;
+bool userInput = int.TryParse(Console.ReadLine(), out num);
+
+if (userInput == false)
+    return;
+
+string binaryValue = "";
+
+while (num > 0)
+{
+    int remainder = num % 2;
+    binaryValue = remainder + binaryValue;
+    num /= 2;
+}
+
+Console.WriteLine(binaryValue);
+Console.ReadKey();
