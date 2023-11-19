42ca76313fe149e6da5abcbf09b03f68fe1dec20
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-19 18:05:11 +0400

    finish Practice_6

---
+++ Day_04/Practice_6/Practice_6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter a number: ");
+int num;
+bool userInput = int.TryParse(Console.ReadLine(), out num);
+
+if (userInput == false)
+    return;
+
+// First divisor of every number is 1
+string divisors = "1";
+
+for (int i = 1; i <= num; i++)
+{
+    if (num % i == 0 && i != 1)
+        divisors += $", {i}";
+}
+
+Console.WriteLine($"Divisors of {num} are: {divisors}");
+Console.ReadKey();
