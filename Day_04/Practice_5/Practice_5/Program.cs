76f2316a94f1dae55e2df12c9524e30c2c60880e
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-19 17:58:48 +0400

    re-write printing logic in Practice_5

--- Day_04/Practice_5/Practice_5/Program.cs
+++ Day_04/Practice_5/Practice_5/Program.cs
@@ -5,35 +5,27 @@ bool userInput = int.TryParse(Console.ReadLine(), out rows);
 if (userInput == false || rows == 0)
     return;
 
-// First row is always 1 in Floyd's triangle
-int firstNum = 1;
-Console.WriteLine(firstNum);
-
-for (int i = 0; i < rows - 1; i++)
+for (int i = 0; i < rows; i++)
 {
-    // If row is odd first number is 1 else it's 0
-    if (i % 2 != 0)
-        firstNum = 1;
-    else
-        firstNum = 0;
+    // First row is always 1 in Floyd's triangle
+    if (i == 0)
+        Console.Write("1");
 
-    Console.Write(firstNum);
-
-    // Print the columns
+    // Print columns
     for (int j = 0; j < i + 1; j++)
     {
-        // If row is even, even columns are 1s, odd columns are 0s
-        // If row is odd, even columns are 0s, odd columns are 1s
-        if (firstNum == 0)
+        // If row is odd, odd columns are 1s and even columns are 0s
+        // If row is even, odd columns are 0s and even columns are 1s
+        if (i % 2 != 0)
         {
-            if (j % 2 == 0)
+            if (j % 2 != 0)
                 Console.Write("1");
             else
                 Console.Write("0");
         }
-        else
+        else if (i % 2 == 0 && i != 0)
         {
-            if (j % 2 == 0)
+            if (j % 2 != 0)
                 Console.Write("0");
             else
                 Console.Write("1");
