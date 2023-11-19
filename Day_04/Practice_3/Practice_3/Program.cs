84c0caf2313fa524909d8d892b79bb3293810765
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-19 16:52:06 +0400

    finish Practice_3

---
+++ Day_04/Practice_3/Practice_3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter a number: ");
+int num;
+bool userInput = int.TryParse(Console.ReadLine(), out num);
+
+if (userInput == false)
+    return;
+
+for (int i = 1; i <= num; i++)
+{
+    int cube = (int)Math.Pow(i, 3);
+    Console.WriteLine($"{i} cubed is {cube}");
+}
+
+Console.ReadKey();
