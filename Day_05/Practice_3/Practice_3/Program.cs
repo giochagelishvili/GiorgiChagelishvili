a365d425075943cf5f7bdf7f0c8b0c2766c9efe1
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-21 17:19:16 +0400

    finish Practice_3

---
+++ Day_05/Practice_3/Practice_3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Enter array size: ");
+int arraySize;
+bool arraySizeInput = int.TryParse(Console.ReadLine(), out arraySize);
+
+if (arraySizeInput == false)
+    return;
+
+int[] numArray = new int[arraySize];
+
+for (int i = 0; i < arraySize; i++)
+{
+    Console.Write($"Enter number for index {i}: ");
+
+    int num;
+    bool numInput = int.TryParse(Console.ReadLine(), out num);
+
+    if (numInput == false) 
+        return;
+
+    numArray[i] = num;
+}
+
+int sum = 0;
+
+foreach (int num in numArray)
+    sum += num;
+
+Console.WriteLine($"Sum of array elements is: {sum}");
+Console.ReadKey();
