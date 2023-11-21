cbc9b6ccca4fc84a874b88409813fbf57c97169f
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-21 17:33:07 +0400

    finish Practice_5

---
+++ Day_05/Practice_5/Practice_5/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Enter array size: ");
+int arraySize;
+bool arraySizeInput = int.TryParse(Console.ReadLine(), out arraySize);
+
+if (arraySizeInput == false || arraySize <= 0)
+    return;
+
+int[] numArray = new int[arraySize];
+
+for (int i = 0; i < arraySize; i++)
+{
+    Console.Write($"Enter number for index {i}: ");
+
+    int num;
+    bool numInput = int.TryParse(Console.ReadLine(), out num);
+
+    if (numInput == false) 
+        return;
+
+    numArray[i] = num;
+}
+
+Console.WriteLine("Unique elements of array are: ");
+
+for (int i = 0; i < arraySize; i++)
+{
+    int currentNum = numArray[i];
+    bool unique = true;
+
+    for (int j = 0; j < arraySize; j++)
+    {
+        if (numArray[j] == currentNum && j != i)
+            unique = false;
+    }
+
+    if (unique == true)
+        Console.WriteLine(currentNum);
+}
+
+Console.ReadKey();
