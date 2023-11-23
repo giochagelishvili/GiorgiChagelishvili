faaad27108e92d1f21cde18b486910312227a65c
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-23 17:49:36 +0400

    finish day 6 Practice_2

---
+++ Day_06/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] numArray = { 1, 3, 123, 15, 13, 23, 98 };
+
+calculateDigitSum(numArray, 6);
+
+Console.ReadKey();
+
+void calculateDigitSum(int[] array, int index)
+{
+    if (index > array.Length - 1 || index < 0)
+        return;
+
+    int num = array[index];
+    int sum = 0;
+
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    Console.WriteLine($"The sum of the digits of a number at index {index} is {sum}");
+}
