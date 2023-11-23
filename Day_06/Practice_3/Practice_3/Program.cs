be2165ff553d8e1936a24026c65ebbff1a5c74ba
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-23 18:13:35 +0400

    finish day 6 Practice_3

---
+++ Day_06/Practice_3/Practice_3/Program.cs
@@ -0,0 +1,52 @@
+﻿int[] numArray = createArray();
+
+if (numArray == null)
+    return;
+
+findMinMax(numArray);
+Console.ReadKey();
+
+int[] createArray()
+{
+    Console.Write("Enter size of array: ");
+
+    int size;
+    bool sizeInput = int.TryParse(Console.ReadLine(), out size);
+
+    if (sizeInput == false || size <= 0)
+        return null;
+
+    int[] numArray = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter integer for index {i}: ");
+
+        int num;
+        bool numInput = int.TryParse(Console.ReadLine(), out num);
+
+        if (numInput == false)
+            return null;
+
+        numArray[i] = num;
+    }
+
+    return numArray;
+}
+
+void findMinMax(int[] numArray)
+{
+    int min = numArray[0];
+    int max = numArray[0];
+
+    foreach (int num in numArray)
+    {
+        if (num < min)
+            min = num;
+        else if (num > max)
+            max = num;
+    }
+
+    Console.WriteLine($"The minimum number in the array is: {min}");
+    Console.WriteLine($"The maximum number in the array is: {max}");
+}
