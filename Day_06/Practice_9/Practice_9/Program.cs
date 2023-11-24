e271cb505486a7346a9bae3a44f80bb29e98aa38
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-24 16:34:25 +0400

    finish day 6 Practice_9

---
+++ Day_06/Practice_9/Practice_9/Program.cs
@@ -0,0 +1,12 @@
+﻿int product = calculateProduct(1, 3, 4, 15, 13, 23, 98);
+Console.WriteLine($"The product of array elements is: {product}");
+
+int calculateProduct(params int[] nums)
+{
+    int result = 1;
+
+    foreach (int num in nums)
+        result *= num;
+
+    return result;
+}
