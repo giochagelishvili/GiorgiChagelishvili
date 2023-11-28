75291c131606dca6f538bd22947f14ee3bdea545
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-28 20:06:20 +0400

    finish Practice_4

---
+++ Day_07/Practice_4/Practice_4/Program.cs
@@ -0,0 +1,18 @@
+﻿string text = "Hello, world!";
+
+printNum(countWords(text));
+
+void printNum(int num)
+{
+    Console.WriteLine(num);
+}
+
+int countWords(string text)
+{
+    if (text == "")
+        return 0;
+
+    string[] words = text.Split(' ');
+
+    return words.Length;
+}
