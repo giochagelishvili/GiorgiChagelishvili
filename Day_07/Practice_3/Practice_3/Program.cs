62f5369571ec442d06e252fe38cd5eae199492f8
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-28 19:44:54 +0400

    finish Practice_3

---
+++ Day_07/Practice_3/Practice_3/Program.cs
@@ -0,0 +1,15 @@
+﻿string text = "Hello, world!";
+
+printWithSpaces(text);
+
+void printWithSpaces(string text)
+{
+    char[] textAsChars = text.ToCharArray();
+
+    foreach(char c in textAsChars)
+    {
+        // print out everything except for spaces (space's ascii value = 32)
+        if (c != 32)
+            Console.Write($"{c} ");
+    }
+}
