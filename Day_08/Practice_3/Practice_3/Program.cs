e596ae9f2b2f00f8f735efacab3d31efbaf96920
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-30 13:26:24 +0400

    finish Practice_3

---
+++ Day_08/Practice_3/Practice_3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(countDigits(999)); // head
+
+Console.WriteLine("------------------------");
+
+Console.WriteLine(countDigitsTail(999)); // tail
+
+// head recursion
+int countDigits(int num)
+{
+    if (num <= 0)
+        return 0;
+    else
+        return 1 + countDigits(num / 10);
+}
+
+// tail recursion
+int countDigitsTail(int num, int digits = 0)
+{
+    if (num <= 0)
+        return digits;
+    else
+        return countDigitsTail(num / 10, ++digits);
+}
