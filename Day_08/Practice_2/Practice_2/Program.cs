4c11692db3959e984555dea322e18c9e4b86551a
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-30 13:03:05 +0400

    finish Practice_2

---
+++ Day_08/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine(calculateSum(13)); // head
+
+Console.WriteLine("------------------------");
+
+Console.WriteLine(calculateSumTail(13, 0)); // tail
+
+// head recursion
+int calculateSum(int num)
+{
+    if (num == 0)
+        return 0;
+    else
+        return num + calculateSum(num - 1);
+}
+
+// tail recursion
+int calculateSumTail(int num, int sum)
+{
+    if (num == 0)
+        return sum;
+    else
+        return calculateSumTail(num - 1, sum + num);
+}
