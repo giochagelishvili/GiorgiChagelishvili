8be6475cf6ca4596d530a418d4b27d8b1be0dd31
Giorgi Chagelishvili <chagx116@gmail.com>
2023-11-30 14:14:38 +0400

    finish Practice_5

---
+++ Day_08/Practice_5/Practice_5/Program.cs
@@ -0,0 +1,22 @@
+﻿bool ana = isPalindrome("ana");
+bool anan = isPalindrome("anana");
+bool aa = isPalindrome("aa");
+bool empty = isPalindrome("");
+
+Console.WriteLine($"Is palindrome? {ana}"); // input - ana
+Console.WriteLine($"Is palindrome? {anan}"); // input - anan
+Console.WriteLine($"Is palindrome? {aa}"); // input - aa
+Console.WriteLine($"Is palindrome? {empty}"); // input - empty string
+
+static bool isPalindrome(string text)
+{
+    int length = text.Length;
+
+    if (length == 1 || length == 0)
+        return true;
+
+    if (text[0] != text[length - 1])
+        return false;
+
+    return isPalindrome(text.Substring(1, length - 2));
+}
