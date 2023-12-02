6911d05b2673ae55878bd467bddccad46d20a103
Giorgi Chagelishvili <chagx116@gmail.com>
2023-12-02 16:24:00 +0400

    finish Midterm 1

---
+++ Midterm 1/2 Task/2 Task/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] numArray = new int[] { 1, 2, 3, 4, 5, 7 };
+Console.WriteLine(findElement(numArray));
+
+int findElement(int[] array)
+{
+    int length = array.Length;
+    int maxNum = length + 1;
+    bool isMissing = true;
+    
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            if (array[j] == maxNum)
+            {
+                isMissing = false;
+                i++;
+                j = 0;
+                maxNum -= 1;
+                continue;
+            }
+        }
+        isMissing = true;
+    }
+
+    if (isMissing == true)
+        return maxNum;
+
+    return 0;
+}
---
+++ Midterm 1/Task 3/Task 3/Program.cs
@@ -0,0 +1,16 @@
+﻿int num = 9;
+Console.WriteLine(toBinary(num));
+
+string toBinary(int num)
+{
+    string result = "";
+
+    while (num > 0)
+    {
+        int remainder = num % 2;
+        result = remainder.ToString() + result;
+        num /= 2;
+    }
+
+    return result;
+}
