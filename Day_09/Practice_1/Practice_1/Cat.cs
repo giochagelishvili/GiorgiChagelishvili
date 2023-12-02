d8f3791f512e847bed0fa875cc41dc52c79fce31
Giorgi Chagelishvili <chagx116@gmail.com>
2023-12-02 22:27:10 +0400

    finish Practice_1

---
+++ Day_09/Practice_1/Practice_1/Cat.cs
@@ -0,0 +1,47 @@
+﻿namespace Practice_1
+{
+    internal class Cat
+    {
+        string? _Name;
+        string? _Breed;
+        string? _Gender;
+        int _Age;
+
+        private int _GramsPerBite = 10;
+
+        public string? Name { get; set; }
+        public string? Breed { get; set; }
+        public string? Gender { get; set; }
+        public int Age 
+        {
+            get { return _Age; }
+            set 
+            {
+                if (value < 0)
+                    Console.WriteLine("Age must be a positive integer or 0.");
+                else
+                    _Age = value;
+            }
+        }
+
+        public void Meow(int count)
+        {
+            if (count <= 0)
+                return;
+            else
+                Console.WriteLine("Meowing ...");
+
+            Meow(count - 1);
+        }
+
+        public void Eat(int grams)
+        {
+            if (grams <= 0)
+                return;
+            else
+                Console.WriteLine("Eating ...");
+
+            Eat(grams - _GramsPerBite);
+        }
+    }
+}
---
+++ Day_09/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Practice_1;
+
+Console.WriteLine("Creating cat object...");
+
+Cat cat = new Cat();
+
+cat.Name = getStringInput("Enter name");
+cat.Breed = getStringInput("Enter breed");
+cat.Gender = getStringInput("Enter sex");
+cat.Age = getIntInput("Enter age");
+
+Console.WriteLine("Cat object created.");
+
+int foodWeight = getIntInput("Enter food weight in grams");
+
+Console.WriteLine($"{cat.Name} started eating.");
+
+cat.Eat(foodWeight);
+
+Console.WriteLine($"{cat.Name} finished eating.");
+
+int meowCount = getIntInput("Enter meowing count");
+cat.Meow(meowCount);
+
+string getStringInput(string message)
+{
+    string? userInput;
+
+    do
+    {
+        Console.Write($"{message}: ");
+        userInput = Console.ReadLine();
+    } while (userInput == null || userInput == "");
+
+    return userInput;
+}
+
+int getIntInput(string message)
+{
+    bool userInput;
+    int value;
+
+    do
+    {
+        Console.Write($"{message}: ");
+        userInput = int.TryParse(Console.ReadLine(), out value);
+    } while (userInput != true);
+
+    return value;
+}
