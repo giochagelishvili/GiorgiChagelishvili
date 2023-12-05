328952e73b47ae0fff648ed17cadda9468ec660c
Giorgi Chagelishvili <chagx116@gmail.com>
2023-12-05 21:39:03 +0400

    finish Practice_1

---
+++ Day_11/Practice_1/Practice_1/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice_1
+{
+    internal class Person
+    {
+        public Person(string name, int age, string idNumber, Home home)
+        {
+            Name = name;
+            Age = age;
+            IdNumber = idNumber;
+            Home = home;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string IdNumber { get; set; }
+        public Home Home { get; set; }
+    }
+}
---
+++ Day_11/Practice_1/Practice_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Practice_1;
+
+Home giorgisHome = new Home("Tbilisi st. 27", "Rustavi");
+Person giorgi = new Person("Giorgi", 23, "1", giorgisHome);
+
+Home lashasHome = new Home("Lasha's st.", "Tbilisi");
+Person lasha = new Person("Lasha", 33, "2", lashasHome);
+
+Home beqasHome = new Home("Beqa's st.", "Tbilisi");
+Person beqa = new Person("Beqa", 31, "3", beqasHome);
+
+Person[] personArray = { giorgi, lasha, beqa };
