1dd804e69964fc3910469264fc2ec48a0ea8da29
Giorgi Chagelishvili <chagx116@gmail.com>
2023-12-06 23:58:38 +0400

    finish Practices

---
+++ Day_12/Practices/Practices/Math.cs
@@ -0,0 +1,72 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Practices
+{
+    public static class MathMethods
+    {
+        enum Statuses
+        {
+            PowMustBeaPositiveOrZero,
+            Success,
+            NumbersMustBeDifferent
+        }
+
+        public static int Pow(int num, int power, out string status)
+        {
+            if (power < 0) 
+            {
+                status = Statuses.PowMustBeaPositiveOrZero.ToString();
+                return -1;
+            }
+
+            int result = 1;
+
+            for (int i = 0; i < power; i++)
+            {
+                result *= num;
+            }
+
+            status = Statuses.Success.ToString();
+            return result;
+        }
+
+        public static int Min(int firstNum, int secondNum, out string status)
+        {
+            if (firstNum == secondNum)
+            {
+                status = Statuses.NumbersMustBeDifferent.ToString();
+                return 0;
+            }
+
+            if (firstNum > secondNum)
+            {
+                status = Statuses.Success.ToString();
+                return secondNum;
+            } else
+            {
+                status = Statuses.Success.ToString();
+                return firstNum;
+            }
+
+        }
+
+        public static int Max(int firstNum, int secondNum, out string status)
+        {
+            if (firstNum == secondNum)
+            {
+                status = Statuses.NumbersMustBeDifferent.ToString();
+                return 0;
+            }
+
+            if (firstNum > secondNum)
+            {
+                status = Statuses.Success.ToString();
+                return firstNum;
+            } else
+            {
+                status = Statuses.Success.ToString();
+                return secondNum;
+            }
+        }
+    }
+}
---
+++ Day_12/Practices/Practices/Program.cs
@@ -0,0 +1,53 @@
+﻿using Practices;
+
+Console.WriteLine("Pow method: ");
+
+int firstNum = GetIntInput("Enter number"); // Get number input from the user
+int secondNum = GetIntInput("Enter power"); // Get power input from the user
+
+string status; // Status will be saved in this variable
+int result = MathMethods.Pow(firstNum, secondNum, out status); // Call Pow() method and save returned value in result
+
+// Print status and result to the user
+Console.Write($"Status: {status} \nResult: {result}\n");
+
+Console.WriteLine("=========================================================="); // Divider
+
+Console.WriteLine("Min method: ");
+
+firstNum = GetIntInput("Enter first number"); // Get first number input from the user
+secondNum = GetIntInput("Enter second number"); // Get second number input from the user
+
+result = MathMethods.Min(firstNum, secondNum, out status); // Call Min() method and save value in result variable
+
+// Print status and result
+Console.Write($"Status: {status} \nResult: {result}\n");
+
+Console.WriteLine("==========================================================");
+
+Console.WriteLine("Max method: ");
+
+firstNum = GetIntInput("Enter first number"); // Get first input 
+secondNum = GetIntInput("Enter second number"); // Get second input
+
+result = MathMethods.Max(firstNum, secondNum, out status); // Call Max() method and save value in result variable
+
+Console.Write($"Status: {status} \nResult: {result}\n");
+
+// Prints the message passed as a parameter
+// Prompts the user for input until user enters an integer value
+// Returns user input
+int GetIntInput(string message)
+{
+    Console.Write($"{message}: ");
+
+    int num;
+    bool userInput;
+
+    do
+    {
+        userInput = int.TryParse(Console.ReadLine(), out num );
+    } while ( userInput != true );
+
+    return num;
+}
