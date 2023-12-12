5bad2ae6dd73ca74a7a49fcaca97e0bf5282270e
Giorgi Chagelishvili <chagx116@gmail.com>
2023-12-13 02:06:09 +0400

    Finish Practice_2

---
+++ Day_13/Practice_2/Practice_2/Consumer.cs
@@ -0,0 +1,136 @@
+﻿namespace Practice_2
+{
+    public class Consumer : Vehicle
+    {
+        private string ConsumerVehicleType { get; set; }
+        protected override string OriginCountry { get; set; }
+        private string Mark {  get; set; }
+        private int ReleaseYear { get; set; }
+
+        public Consumer()
+        {
+            ConsumerVehicleType = ChooseType();
+            OriginCountry = GetOriginCountry();
+            Mark = GetMark();
+            ReleaseYear = GetReleaseYear();
+            PrintFinalResult();
+        }
+
+        private int GetReleaseYear()
+        {
+            int releaseYear = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Enter release year: ");
+                Console.WriteLine("Type \"info\" to see info.");
+
+                string userInput = Console.ReadLine();
+
+                if (userInput == "info")
+                    PrintInfo();
+                else
+                {
+                    releaseYear = int.Parse(userInput);
+                    break;
+                }
+            }
+
+            return releaseYear;
+        }
+
+        private string GetMark()
+        {
+            string mark;
+
+            while (true)
+            {
+                Console.WriteLine("Enter vehicle mark: ");
+                Console.WriteLine("Type \"info\" to see info.");
+
+                mark = Console.ReadLine();
+
+                if (mark == "info")
+                    PrintInfo();
+                else
+                    break;
+            }
+
+            return mark;
+        }
+
+        protected override string GetOriginCountry()
+        {
+            string originCountry;
+
+            while (true)
+            {
+                Console.WriteLine("Enter origin country: ");
+                Console.WriteLine("Type \"info\" to see info.");
+
+                originCountry = Console.ReadLine();
+
+                if (originCountry == "info")
+                    PrintInfo();
+                else
+                    break;
+            }
+
+            return originCountry;
+        }
+
+        protected override string ChooseType()
+        {
+            int index = 0;
+            string chosenType = "";
+
+            while (true)
+            {
+                Console.WriteLine("Choose consumer vehicle type:");
+                Console.WriteLine("Type \"info\" to see info.");
+
+                for (int i = 1; i <= 4; i++)
+                {
+                    ConsumerTypes currentType = (ConsumerTypes)i;
+                    Console.WriteLine($"{i}. {currentType}");
+                }
+
+                string userInput = Console.ReadLine();
+
+                if (userInput == "info")
+                    PrintInfo();
+                else
+                {
+                    index = int.Parse(userInput);
+                    ConsumerTypes currentType = (ConsumerTypes)index;
+                    chosenType = currentType.ToString();
+                    break;
+                }
+            }
+
+            return chosenType;
+        }
+
+        protected override void PrintInfo()
+        {
+            Console.WriteLine($"Vehicle type: {ConsumerVehicleType}");
+            Console.WriteLine($"Origin country: {OriginCountry}");
+            Console.WriteLine($"Mark: {Mark}");
+            Console.WriteLine($"Release year: {ReleaseYear}");
+        }
+
+        protected override void PrintFinalResult()
+        {
+            Console.WriteLine("Final result:");
+            PrintInfo();
+        }
+
+        enum ConsumerTypes
+        {
+            Sedan = 1,
+            SUV,
+            SuperCar,
+            Minivan
+        }
+    }
+}
---
+++ Day_13/Practice_2/Practice_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using Practice_2;
+
+string vehicleType = GetTypeInput();
+
+Vehicle vehicle = CreateObject(vehicleType);
+
+Vehicle CreateObject(string type)
+{
+    switch(type)
+    {
+        case "Combat":
+            return new Combat();
+        case "Consumer":
+            return new Consumer();
+        case "Public":
+            return new Public();
+        case "Sport":
+            return new Sport();
+        default:
+            return null;
+    }
+}
+string GetTypeInput()
+{
+    Console.WriteLine("Choose type of vehicle:");
+
+    for (int i = 1; i <= 4; i++)
+    {
+        VehicleTypes currentType = (VehicleTypes)i;
+        Console.WriteLine($"{i}. {currentType}");
+    }
+
+    int typeIndex;
+    bool typeInput = false;
+
+    do
+    {
+        int.TryParse(Console.ReadLine(), out typeIndex);
+    } while (typeInput != true && (typeIndex <= 0 || typeIndex > 4));
+
+    VehicleTypes chosenType = (VehicleTypes)typeIndex;
+
+    return chosenType.ToString();
+}
+enum VehicleTypes
+{
+    Combat = 1,
+    Consumer,
+    Public,
+    Sport
+}
