b08563d467764077d6b92620d5d9d473ca94e424
Giorgi Chagelishvili <chagx116@gmail.com>
2023-12-19 17:38:06 +0400

    Finish Data Structures Practices

---
+++ Day_17/Data Structures Practices/Data Structures Practices/Classes/Book.cs
@@ -0,0 +1,20 @@
+﻿namespace Data_Structures_Practices.Classes
+{
+    public class Book
+    {
+        public string? Author { get; set; }
+        public string? Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public string? ISBN { get; set; }
+        public Enum? Genre { get; set; }
+
+        public Book(string author, string title, int releaseYear, string ISBN, Enum genre)
+        {
+            Author = author;
+            Title = title;
+            ReleaseYear = releaseYear;
+            this.ISBN = ISBN;
+            Genre = genre;
+        }
+    }
+}
---
+++ Day_17/Data Structures Practices/Data Structures Practices/Classes/MultipleReturnValues.cs
@@ -0,0 +1,16 @@
+﻿namespace Data_Structures_Practices.Classes
+{
+    public static class MultipleReturnValues
+    {
+        public static Tuple<int, bool> findMin(int firstNum, int secondNum)
+        {
+            if (firstNum == secondNum)
+                return new Tuple<int, bool>(firstNum, false);
+
+            if (firstNum < secondNum)
+                return new Tuple<int, bool>(firstNum, true);
+
+            return new Tuple<int, bool>(secondNum, true);
+        }
+    }
+}
