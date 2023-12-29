0dddaf87d0439f2c31b703b4e41b32029ac5d8bf
Giorgi Chagelishvili <chagx116@gmail.com>
2023-12-29 17:27:52 +0400

    Finish Testing Time

---
+++ Day_21/Testing Time/Testing Time/Exceptions/MustHaveTextException.cs
@@ -0,0 +1,9 @@
+﻿namespace Testing_Time.Exceptions
+{
+    internal class MustHaveTextException : Exception
+    {
+        public MustHaveTextException(string? message = "Please provide the text.") : base(message)
+        {
+        }
+    }
+}
