07e4504059a322b832d9b59a45c881828ce2deee
Giorgi Chagelishvili <chagx116@gmail.com>
2024-04-06 19:58:21 +0400

    Implement login and registration for MVC

---
+++ Forum.ItAcademy.Ge/Forum/Forum.Application/Accounts/LoginRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Forum.Application.Accounts
+{
+    public class LoginRequestModel
+    {
+        public string Username { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public bool RememberLogin { get; set; }
+    }
+}
